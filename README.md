014ef0c8611e9168ea5b723fd61cf3812d49687f
lvlahos <lvlahos343@gmail.com>
2021-08-20 16:40:50 -0400

    added README

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# pyther
-Porting of Protein Activity and Pathway Inference to single cell and Python.
+Here is a brief guide on how to use the current version of the pyther code:
+
+1 - Data Preparation
+
+Start by saving your gene expression signature (GES) and interactome object as .tsv files. For the interactome, you should use the `InteractomeToTable` function in the `r-helper.R` script.
+
+2 - Prepare your environment.
+
+Set your path variables and source in the code as follows:
+
+```python
+path_str = "PATH_TO_PYTHER"
+os.chdir(path_str)
+
+from pyther_classes import *
+from pyther_fn import *
+```
+
+3 - Load Data
+
+Load your data as follows (presuming they are in the same directory as pyther code, modify your script as necessary):
+
+```python
+gesObj = anndata.read_csv('ges.tsv', delimiter = '\t')
+intObj = InteractomefromTSV('network.tsv', 'network_name')
+
+```
+
+4 - Run aREA Function
+
+Run aREA. This is equivalent to running Viper with `eset.filter = FALSE` and `method = 'none'`. In future updates, we will include the sample-shuffling null model that is also available in VIPER for group-vs-group comparisons.
+
+```python
+nesMat = aREA(gesObj, intObj)
+
+```
--- pyther_fn.py
+++ pyther_fn.py
@@ -25,32 +25,47 @@ def InteractomefromTSV(filePath, intName):
 
 def aREA(ges, intObj):
     gesMat = ges.X
-    # transform the GES
-    ges2t = rankdata(gesMat, axis = 0) / (gesMat.shape[0] + 1)
-    ges2tQ = norm.ppf(ges2t)
-    ges1t = abs(ges2t - 0.5) * 2
-    ges1t = ges1t + (1 - np.max(ges1t)) / 2
-    ges1tQ = norm.ppf(ges1t)
     
-    # get matrices out of interactome
-    icMat = intObj.icMat()
-    morMat = intObj.morMat()
-
-    # find the intersecting genes
-    # TO DO: flag for not enough genes
+    # rank transform the GES
+    rankMat = rankdata(gesMat, axis = 0)
+    
+    # find intersecting genes
     targetSet = intObj.get_targetSet()
-    obsNames = ges.obs_names.to_list()
+    obsNames = gesObj.obs_names.to_list()
     intersectGenes = [value for value in targetSet if value in obsNames]
-    targetInds = [obsNames.index(i) for i in intersectGenes]
+    
+    # reduce regulon matrices
+    icMat = intObj.icMat().loc[intersectGenes]
+    morMat = intObj.morMat().loc[intersectGenes]
+    print(icMat.shape)
+    print(morMat.shape)
+    
+    # prepare the 1-tailed / 2-tailed matrices
+    gesInds = [obsNames.index(i) for i in intersectGenes]
+    ges2T = rankMat / (rankMat.shape[0] + 1)
+    ges1T = abs(ges2T - 0.5) * 2
+    ges1T = ges1T + (1 - np.max(ges1T))/2
+    ges2TQ = norm.ppf(ges2T[gesInds, :])
+    ges1TQ = norm.ppf(ges1T[gesInds, :])
+    print(ges2TQ)
+    print(ges1TQ)
     
     # 2-tail enrichment
     dES = pd.DataFrame.transpose(pd.DataFrame.mul(icMat, morMat))
-    dES = dES.dot(ges2tQ[targetInds, :])
+    dES = dES.dot(ges2TQ)
+    print(dES.shape)
+    print(dES)
+    
     # 1-tail enrichemnt
     uES = pd.DataFrame.transpose(pd.DataFrame.mul(1 - abs(morMat), icMat))
-    uES = uES.dot(ges1tQ[targetInds, :])
+    uES = uES.dot(ges1TQ)
+    print(uES.shape)
+    print(uES)
+    
     # integrate
     iES = (abs(dES) + uES * (uES > 0)) * np.sign(dES)
+    print(iES.shape)
+    print(iES)
     # make NES
     nES = iES.mul(intObj.icpVec(), 0)
     
