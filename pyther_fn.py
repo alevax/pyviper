1650d1f3906c619aae2ebb33f1794afa9ed15f46
lvlahos <lvlahos343@gmail.com>
2021-10-16 14:53:09 -0400

    adjusted aREA for transposed convention

--- pyther_fn.py
+++ pyther_fn.py
@@ -25,49 +25,40 @@ def InteractomefromTSV(filePath, intName):
 
 def aREA(gesObj, intObj):
     gesMat = gesObj.X
-    
+        
     # rank transform the GES
-    rankMat = rankdata(gesMat, axis = 0)
-    
+    rankMat = rankdata(gesMat, axis = 1)
+        
     # find intersecting genes
     targetSet = intObj.get_targetSet()
-    obsNames = gesObj.obs_names.to_list()
-    intersectGenes = [value for value in targetSet if value in obsNames]
-    
+    varNames = gesObj.var_names.to_list()
+    intersectGenes = [value for value in targetSet if value in varNames]
+        
     # reduce regulon matrices
     icMat = intObj.icMat().loc[intersectGenes]
     morMat = intObj.morMat().loc[intersectGenes]
-    print(icMat.shape)
-    print(morMat.shape)
-    
+        
     # prepare the 1-tailed / 2-tailed matrices
-    gesInds = [obsNames.index(i) for i in intersectGenes]
-    ges2T = rankMat / (rankMat.shape[0] + 1)
+    gesInds = [varNames.index(i) for i in intersectGenes]
+    ges2T = rankMat / (rankMat.shape[1] + 1)
     ges1T = abs(ges2T - 0.5) * 2
     ges1T = ges1T + (1 - np.max(ges1T))/2
-    ges2TQ = norm.ppf(ges2T[gesInds, :])
-    ges1TQ = norm.ppf(ges1T[gesInds, :])
-    print(ges2TQ)
-    print(ges1TQ)
-    
+    ges2TQ = norm.ppf(ges2T[:, gesInds])
+    ges1TQ = norm.ppf(ges1T[:, gesInds])
+        
     # 2-tail enrichment
     dES = pd.DataFrame.transpose(pd.DataFrame.mul(icMat, morMat))
-    dES = dES.dot(ges2TQ)
-    print(dES.shape)
-    print(dES)
-    
+    dES = dES.dot(np.transpose(ges2TQ))
+        
     # 1-tail enrichemnt
     uES = pd.DataFrame.transpose(pd.DataFrame.mul(1 - abs(morMat), icMat))
-    uES = uES.dot(ges1TQ)
-    print(uES.shape)
-    print(uES)
-    
+    uES = uES.dot(np.transpose(ges1TQ))
+        
     # integrate
     iES = (abs(dES) + uES * (uES > 0)) * np.sign(dES)
-    print(iES.shape)
-    print(iES)
     # make NES
     nES = iES.mul(intObj.icpVec(), 0)
+    nES = np.transpose(nES)
     
     return(nES)
 
