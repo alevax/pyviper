7c1e04fe2d98defac246212ef1dd35baad2616d6
Alessandro Vasciaveo <alevax@gmail.com>
2023-07-18 10:03:48 -0400

    Update r-helper.R

--- libs/r-helper.R
+++ libs/r-helper.R
@@ -3,22 +3,32 @@
 #' @param out.file File path to write out as .tsv.
 #' @return If no outfile, returns the network as a data frame.
 InteractomeToTable <- function(net.obj, out.file) {
-  # make df
-  net.df <- do.call(rbind, lapply(names(net.obj), function(x) {
-    reg.obj <- net.obj[[x]] 
-    reg.df <- data.frame('regulator' = rep(x, length(reg.obj$tfmode)),
-                         'target' = names(reg.obj$tfmode),
-                         'mor' = reg.obj$tfmode,
-                         'likelihood' = reg.obj$likelihood)
-    rownames(reg.df) <- NULL
-    return(reg.df)
-  }))
-  # if specified, write to file
-  if (!missing(out.file)) {
-    write.table(net.df, file = out.file, quote = FALSE, sep = '\t',
-                row.names = FALSE, col.names = TRUE)
-  } else {
-    return(reg.df)
-  }
+	# make df
+	net.df <- do.call(rbind, lapply(names(net.obj), function(x) {
+		reg.obj <- net.obj[[x]] 
+		if (is.na(reg.obj$subnets))
+		{
+			reg.df <- data.frame('regulator' = rep(x, length(reg.obj$tfmode)),
+													 'target' = names(reg.obj$tfmode),
+													 'mor' = reg.obj$tfmode,
+													 'likelihood' = reg.obj$likelihood)
+		} else {
+			reg.df <- data.frame('regulator' = rep(x, length(reg.obj$tfmode)),
+													 'target' = names(reg.obj$tfmode),
+													 'mor' = reg.obj$tfmode,
+													 'likelihood' = reg.obj$likelihood,
+													 'subnets' = reg.obj$subnets)
+		}
+		
+		rownames(reg.df) <- NULL
+		return(reg.df)
+	}))
+	# if specified, write to file
+	if (!missing(out.file)) {
+		write.table(net.df, file = out.file, quote = FALSE, sep = '\t',
+								row.names = FALSE, col.names = TRUE)
+	} else {
+		return(reg.df)
+	}
 }
 
