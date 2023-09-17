ca82bf203013c8ef35a1d6967c082fd19090dab6
Alec Wang <alewang99@gmail.com>
2023-09-17 12:25:01 -0400

    Updates narnea to work with Interactome class & increased efficiency for parallelization

--- libs/pyther_narnea.py
+++ libs/pyther_narnea.py
@@ -85,13 +85,13 @@ def nes_covariance(rank_mat, sign_mat,AW_AM_prob, AW_AM_abs_prob, E_r, E_rs, D_V
     E_du = cov_prod_mat.transpose() @ E_r2s
     E_d_u = cov_prod_mat.transpose() @ np.matrix(E_rs * E_r)
     COV_du = E_du - E_d_u
-    
+
     COV_nes = COV_du/np.sqrt(np.multiply(D_V , U_V))
     return(COV_nes)
 
 
 def combine_nes(D_nes, U_nes, COV_nes):
-    
+
     NES_pos = (D_nes + U_nes) / np.sqrt(2 + 2 * COV_nes)
     NES_neg = (D_nes - U_nes) / np.sqrt(2 - 2 * COV_nes)
 
@@ -103,7 +103,7 @@ def combine_nes(D_nes, U_nes, COV_nes):
     final_p = min_p + np.log(2) + np.log1p(np.exp(min_p) / (-2))
 
     # facing precision issue with this approach, switch to a numerical approximation way
-     
+
     # quantile =  np.exp(final_p - np.log(2))
     # pos_nes = norm.isf(quantile, loc=0, scale=1)
     # neg_nes = norm.ppf(quantile, loc=0, scale=1)
@@ -115,7 +115,7 @@ def combine_nes(D_nes, U_nes, COV_nes):
 #   pos_nes = vect_quantile(a)
 #
     neg_nes = -pos_nes
-    
+
 
 
 
@@ -126,7 +126,7 @@ def combine_nes(D_nes, U_nes, COV_nes):
 def replace_random(x, a, b):
     if x == 0:
         rand = np.random.random()# 0,1
-        
+
         return rand*(b-a) + a
     else:
         return x
@@ -134,28 +134,30 @@ def replace_random(x, a, b):
 
 ### Matrix narnea
 
-def matrix_narnea(gesObj, int_table, intermediate = False, min_targets = 30):
-    
+def matrix_narnea(gesObj, intObj, intermediate = False, min_targets = 30):
+
+    int_table = intObj.netTable
+
     pd.options.mode.chained_assignment = None
     exp_genes = list(gesObj.var.sort_index().index)
 
 
     # modify regulon list, take the intersect of targets and genes, making there is no 0 nor +_1 in am
 
-    filtered_table = int_table[int_table['target'].isin(exp_genes)] 
+    filtered_table = int_table[int_table['target'].isin(exp_genes)]
     # why I need to remove that, there is no filtered_table[filtered_table['mor'] == -1]
     filtered_table['mor'].replace(1 ,0.999, inplace= True)
     filtered_table['mor'].replace(-1 ,-0.999, inplace= True)
     filtered_table['mor'] = filtered_table['mor'].apply(lambda x: replace_random(x, -0.001, 0.001))
-    #filtered_table['mor'].replace(0 ,0.999, inplace= True) 
+    #filtered_table['mor'].replace(0 ,0.999, inplace= True)
     # why we cant have 0, 1 and -1?
 
     # filtered out those with target less than min.targets )may be this should  be a function for interactone class
-    reg_counts  = filtered_table['regulator'].value_counts() 
+    reg_counts  = filtered_table['regulator'].value_counts()
     reg_counts = list(reg_counts[reg_counts>=min_targets].index)
-    filtered_table = filtered_table[filtered_table['regulator'].isin(reg_counts)] 
+    filtered_table = filtered_table[filtered_table['regulator'].isin(reg_counts)]
 
-    # modify the expression matrix: 
+    # modify the expression matrix:
         # fill with number less than the min in this sample, sign is randomly assigned according to the +_ proportion
 
     expr = np.copy(gesObj[:,exp_genes].X)
@@ -184,14 +186,14 @@ def matrix_narnea(gesObj, int_table, intermediate = False, min_targets = 30):
     signs = np.sign(expr)
 
 
-    # initializing matrixs with 
+    # initializing matrixs with
     genes = list(gesObj.var.index)
     regulons = list(filtered_table['regulator'].drop_duplicates())
 
     # wait, not necesscarily do that, can we do sth like merge and pivot longer?
     dat1 = pd.DataFrame({'target':genes})
 
-    dat2 = dat1.merge(filtered_table, on='target', how = 'left') 
+    dat2 = dat1.merge(filtered_table, on='target', how = 'left')
     # should I use left or inner? #no. have to use left join
     #dat2.dropna(inplace= True)
     AM_mat = dat2.pivot(index='target',columns='regulator',values = 'mor').fillna(0)
@@ -229,7 +231,7 @@ def matrix_narnea(gesObj, int_table, intermediate = False, min_targets = 30):
     print('Calculating PES...')
 
 
-    # for each regulator, calculate two values  
+    # for each regulator, calculate two values
 
     filtered_table['rank'] = filtered_table.groupby('regulator')['likelihood'].rank(method = 'dense', ascending = False)
     filtered_table['rank'] = len(genes) - filtered_table['rank'] + 1
@@ -241,7 +243,7 @@ def matrix_narnea(gesObj, int_table, intermediate = False, min_targets = 30):
 
     #max_du = pd.concat([filtered_table.groupby(['regulator'])['d'].sum(), filtered_table.groupby(['regulator'])['u'].sum()], axis=1)
     #max_du
-    # if I want it I can directly create the two matrix 
+    # if I want it I can directly create the two matrix
     PES_pos_D  = filtered_table.groupby(['regulator'])['d'].sum().values
     PES_pos_D = PES_pos_D[:, np.newaxis] * np.ones(expr.shape[0])[np.newaxis, :]
 
@@ -249,40 +251,48 @@ def matrix_narnea(gesObj, int_table, intermediate = False, min_targets = 30):
     PES_pos_U = filtered_table.groupby(['regulator'])['u'].sum().values
     PES_pos_U = PES_pos_U[:, np.newaxis] * np.ones(expr.shape[0])[np.newaxis, :]
 
-    PES_pos_D_nes = (PES_pos_D - D_list[1])/np.sqrt(D_list[2]) 
+    PES_pos_D_nes = (PES_pos_D - D_list[1])/np.sqrt(D_list[2])
     PES_pos_U_nes = (PES_pos_U - U_list[1])/np.sqrt(U_list[2])
     PES_pos_NES = combine_nes(PES_pos_D_nes, PES_pos_U_nes, COV_nes)
 
     PES_neg_D = PES_pos_D * (-1)
     PES_neg_U = PES_pos_U
-    PES_neg_D_nes = (PES_neg_D - D_list[1])/np.sqrt(D_list[2]) 
+    PES_neg_D_nes = (PES_neg_D - D_list[1])/np.sqrt(D_list[2])
     PES_neg_U_nes = (PES_neg_U - U_list[1])/np.sqrt(U_list[2])
     PES_neg_NES = combine_nes(PES_neg_D_nes, PES_neg_U_nes, COV_nes)
 
-    pos_NES = (NES_mat > 0) 
+    pos_NES = (NES_mat > 0)
     PES_comb_nes = PES_pos_NES * pos_NES + PES_neg_NES * (~pos_NES)
     PES_mat = NES_mat / np.abs(PES_comb_nes)
 
-    
+
 
     if (intermediate) == False :
-        NES_mat = pd.DataFrame(NES_mat.T, index = gesObj.obs.index, columns= list(AW_AM_prob.columns))
-        PES_mat = pd.DataFrame(PES_mat.T, index = gesObj.obs.index, columns= list(AW_AM_prob.columns))
-                               
+        NES_mat = pd.DataFrame(NES_mat.T, index = gesObj.obs.index, columns = list(AW_AM_prob.columns))
+        PES_mat = pd.DataFrame(PES_mat.T, index = gesObj.obs.index, columns = list(AW_AM_prob.columns))
 
-    
-    result = [NES_mat, PES_mat]
+
+
+    result = {"nes": NES_mat, "pes": PES_mat}
     return result
 
 ### Meta narnea
 
-def meta_narnea(gesObj, intList, sample_weight = True, njobs = 3):
+def meta_narnea(gesObj, intObj, sample_weight = True, njobs = 1):
     pd.options.mode.chained_assignment = None
-    results = Parallel(n_jobs = njobs)(
-        (delayed)(matrix_narnea)(gesObj,iObj)
-        for iObj in intList
-        )
-    
+
+    if intObj is Interactome:
+        return matrix_narnea(gesObj, intObj)
+    elif len(intObj) == 1:
+        return matrix_narnea(gesObj, intObj[0])
+    elif njobs == 1:
+        results = [matrix_narnea(gesObj, iObj) for iObj in intObj]
+    else:
+        results = Parallel(n_jobs = njobs)(
+            (delayed)(matrix_narnea)(gesObj,iObj)
+            for iObj in intObj
+            )
+
     the_first = results.pop(0)
     melt_pes = the_first[1].reset_index().melt(
         id_vars = 'index',
@@ -299,7 +309,7 @@ def meta_narnea(gesObj, intList, sample_weight = True, njobs = 3):
     melt_pes.dropna(inplace=True)
     melt_pes
 
-    pes_max = np.apply_along_axis(np.argmax, axis=1, arr=np.abs(melt_pes[list(melt_pes.columns)[2:]])) 
+    pes_max = np.apply_along_axis(np.argmax, axis=1, arr=np.abs(melt_pes[list(melt_pes.columns)[2:]]))
 
     net_weight = melt_pes[['index']]
     net_weight['net'] = pes_max
@@ -307,7 +317,7 @@ def meta_narnea(gesObj, intList, sample_weight = True, njobs = 3):
 
     net_weight = net_weight.pivot(index = 'level_0', columns = 'net',values = 'net' ).notna()
     net_weight['index'] = melt_pes[['index']]
-    
+
     shared_regs = melt_pes['gene'].drop_duplicates()
     net_weight = net_weight.groupby('index').sum()/len(shared_regs)
 
@@ -321,14 +331,14 @@ def meta_narnea(gesObj, intList, sample_weight = True, njobs = 3):
     all_regs_pes.fillna(0, inplace = True)
     all_regs_pes.sort_index(inplace=True)
     pes_dom = (all_regs_pes != 0) * net_weight[0].to_numpy()[:, np.newaxis] * np.ones((1,len(all_regs)))
-    
+
     net_weight_array = net_weight[0].to_numpy()[:, np.newaxis] * np.ones((1,len(all_regs)))
     net_weight_array
     pre_pes = pre_pes + all_regs_pes * net_weight_array
     pre_pes
 
     for i in range(1,len(results)+1):
-        all_regs_pes =  bg_matrix + results[i-1][1] 
+        all_regs_pes =  bg_matrix + results[i-1][1]
         all_regs_pes.fillna(0, inplace = True)
         all_regs_pes.sort_index(inplace=True)
         pes_dom = pes_dom + (all_regs_pes!= 0) * net_weight[i].to_numpy()[:, np.newaxis] * np.ones((1,len(all_regs)))
@@ -339,7 +349,7 @@ def meta_narnea(gesObj, intList, sample_weight = True, njobs = 3):
 
     # NES
 
-    pre_nes = bg_matrix.copy() 
+    pre_nes = bg_matrix.copy()
     all_regs_nes = bg_matrix + the_first[0]
     all_regs_nes.fillna(0, inplace = True)
     all_regs_nes.sort_index(inplace=True)
@@ -352,7 +362,7 @@ def meta_narnea(gesObj, intList, sample_weight = True, njobs = 3):
 
 
     for i in range(1,len(results)+1):
-        all_regs_nes =  bg_matrix + results[i-1][0] 
+        all_regs_nes =  bg_matrix + results[i-1][0]
         all_regs_nes.fillna(0, inplace = True)
         all_regs_nes.sort_index(inplace=True)
         nes_dom = nes_dom + ((all_regs_pes!= 0) * net_weight[i].to_numpy()[:, np.newaxis] * np.ones((1,len(all_regs))))**2
@@ -360,11 +370,7 @@ def meta_narnea(gesObj, intList, sample_weight = True, njobs = 3):
         pre_nes =  pre_nes + all_regs_nes * net_weight_array
 
     pre_nes = pre_nes/np.sqrt(nes_dom)
-    pre_nes
 
-    # anndata op
 
-    op = anndata.AnnData(pre_nes)
-    op.layers['pes'] = pre_pes
-
-    return op
+    result = {"nes": pre_nes, "pes": pre_pes}
+    return result
