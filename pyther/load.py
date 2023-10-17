d5d4cc9eb47bbc8b06b916a67b51013d034d7d61
MiqG <miquelangladagirotto@gmail.com>
2023-10-17 17:22:54 +0200

    docs as gh-pages

--- pyther/load.py
+++
@@ -1,8 +0,0 @@
-### Import dependencies
-from ._load._load import *
-
-### EXPORT LIST
-# __all__ = ['mouse2human', 'human2mouse',
-#            'TFs', 'coTFs', 'sig', 'surf',
-#            'msigdb_regulon']
-__all__ = []
