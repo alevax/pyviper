65389c0185bbb6bbfaf0419f2707ad3853812198
LucaZanella15 <lz2841@cumc.columbia.edu>
2024-04-21 20:12:12 -0400

    data in pyviper and manifest and setup.py for pypi

--- setup.py
+++ setup.py
@@ -5,12 +5,15 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="viper-in-python",
-    version="1.0.4",
+    version="1.1.7",
     author="Alexander L.E. Wang & Zizhao Lin & Luca Zanella",
     author_email="aw3436@cumc.columbia.edu",
     packages=setuptools.find_packages(),
-    description="A package to load ARACNe networks and to run VIPER and NaRnEA in Python",
-    long_description="This Scanpy-compatible package provides the Interactome class to easily allow the user to both load and interact with ARACNe networks. Once these networks have been loaded, the user has the option to filter regulators, filter targets, and prune the network. The user can then give their Interactome (or multiple Interactomes) and a gene expression signature to Pyviper function to either run VIPER or NaRnEA to infer protein activity. This package also provides the option to compute pathway enrichment with Interactomes built from sets of pathways from MSigDB.",
+    package_data={'pyviper': ['data/*']},  # Include data directory
+    #package_data={'': ['data/*']}, # Include data directory
+    include_package_data=True,
+    description="A package for VIPER-based Protein Activity analysis of transcriptomic data in Python",
+    long_description="This Scanpy-compatible package provides the Interactome class to easily allow the user to both load and interact with gene regulatory networks. Once these networks have been loaded, the user has the option to filter regulators, filter targets, and prune the network. The user can then give their Interactome (or multiple Interactomes) and a gene expression signature to the viper function to run VIPER (aREA or NaRnEA enrichment methods) to infer protein activity. This package also provides the option to compute pathway enrichment with Interactomes built from sets of pathways from MSigDB and several modules for data processing.",
     long_description_content_type="text/markdown",
     url="https://github.com/alevax/pyviper",
     license='MIT',
